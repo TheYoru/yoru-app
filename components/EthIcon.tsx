506189332f3f444e0b44c7dcac47055aa938d4d4
Randy Lien <randylien@gmail.com>
2023-04-15 20:32:39 +0900

    update icons

--- components/EthIcon.tsx
+++ components/EthIcon.tsx
@@ -7,11 +7,11 @@ export const EthIcon = (args) => {
         height="100%"
         width="100%"
         version="1.1"
-        shape-rendering="geometricPrecision"
-        text-rendering="geometricPrecision"
-        image-rendering="optimizeQuality"
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        shapeRendering="geometricPrecision"
+        textRendering="geometricPrecision"
+        imageRendering="optimizeQuality"
+        fillRule="evenodd"
+        clipRule="evenodd"
         viewBox="0 0 784.37 1277.39"
       >
         <g id="Layer_x0020_1">
@@ -20,32 +20,32 @@ export const EthIcon = (args) => {
             <g>
               <polygon
                 fill="#343434"
-                fill-rule="nonzero"
+                fillRule="nonzero"
                 points="392.07,0 383.5,29.11 383.5,873.74 392.07,882.29 784.13,650.54 "
               />
               <polygon
                 fill="#8C8C8C"
-                fill-rule="nonzero"
+                fillRule="nonzero"
                 points="392.07,0 -0,650.54 392.07,882.29 392.07,472.33 "
               />
               <polygon
                 fill="#3C3C3B"
-                fill-rule="nonzero"
+                fillRule="nonzero"
                 points="392.07,956.52 387.24,962.41 387.24,1263.28 392.07,1277.38 784.37,724.89 "
               />
               <polygon
                 fill="#8C8C8C"
-                fill-rule="nonzero"
+                fillRule="nonzero"
                 points="392.07,1277.38 392.07,956.52 -0,724.89 "
               />
               <polygon
                 fill="#141414"
-                fill-rule="nonzero"
+                fillRule="nonzero"
                 points="392.07,882.29 784.13,650.54 392.07,472.33 "
               />
               <polygon
                 fill="#393939"
-                fill-rule="nonzero"
+                fillRule="nonzero"
                 points="0,650.54 392.07,882.29 392.07,472.33 "
               />
             </g>
