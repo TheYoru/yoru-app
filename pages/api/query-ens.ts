aad33d9dba31d75fd40a2d275269304c9ee50e70
Charles Jhong <charlesjhongc@gmail.com>
2023-04-15 23:22:36 +0800

    re-format pages

--- pages/api/query-ens.ts
+++ pages/api/query-ens.ts
@@ -1,15 +1,12 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import { getAddrFromEns } from "./ens";
+import type { NextApiRequest, NextApiResponse } from "next"
+import { getAddrFromEns } from "./ens"
 
 type Data = {
-  name: string;
-};
+    name: string
+}
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  const result = await getAddrFromEns("randyplayerone.eth");
-  console.log(result);
-  res.status(200).json({ name: "John Doe" });
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+    const result = await getAddrFromEns("randyplayerone.eth")
+    console.log(result)
+    res.status(200).json({ name: "John Doe" })
 }
