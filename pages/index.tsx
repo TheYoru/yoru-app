2310670a95ee309127f249f35003edbbe6916158
Randy Lien <randylien@gmail.com>
2023-04-16 14:42:12 +0900

    add color on the text

--- pages/index.tsx
+++ pages/index.tsx
@@ -250,7 +250,8 @@ export default function Home() {
       }}
     >
       <div className="z-10 items-center font-mono text-sm lg:flex mb-2">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
+        style={{"color": "#eee"}}>
           Yoru send funds secretly...
         </p>
       </div>
